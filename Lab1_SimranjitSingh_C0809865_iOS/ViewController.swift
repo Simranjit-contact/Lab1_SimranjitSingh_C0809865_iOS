3105f8462b634f32590e8a5ddac12461f49aca73
simranjit-contact <C0809865@mylambton.ca>
2022-01-19 06:57:57 +0800

    Initial Commit

---
+++ Lab1_SimranjitSingh_C0809865_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_SimranjitSingh_C0809865_iOS
+//
+//  Created by user213023 on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
